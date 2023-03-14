d3a64abb72e5af329b0c65e1e93751388f74afdb
Lachlan Wilger <lachlan@wilgerfamily.com>
2023-03-13 18:03:31 -0700

    Create UserData struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,63 @@
+use std::error::Error;
+use uuid::Uuid;
+
+pub struct CheeseRating;
+pub struct CheeseRatingRequest;
+pub struct CheeseRegistry;
+pub struct UserCheeseRating<'a>(pub &'a str, pub CheeseRatingRequest);
+pub struct RegistryCheeseRating<'a>(pub &'a str, pub CheeseRatingRequest);
+
+pub struct UserData<'a> {
+    id: Uuid,
+    name: &'a str,
+    age: u8,
+}
+
+impl<'a> UserData<'a> {
+    pub fn new() -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name: "",
+            age: 0,
+        }
+    }
+
+    // Constructors for unit testing
+    fn name(self, name: &'a str) -> Self {
+        Self { name, ..self }
+    }
+
+    fn age(self, age: u8) -> Self {
+        Self { age, ..self }
+    }
+}
+
+pub fn rate_cheese(
+    request: CheeseRatingRequest,
+    user: &mut UserData,
+    registry: &mut CheeseRegistry,
+) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
 #[cfg(test)]
-pub mod test_helpers;
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_user_data() {
+        let user = UserData::new().name("Jeffery Hugo").age(18);
+
+        assert_eq!(user.name, "Jeffery Hugo");
+        assert_eq!(user.age, 18);
+        assert!(!user.id.is_nil());
+    }
+
+    #[test]
+    fn new_user_has_unique_id() {
+        let user_1 = UserData::new();
+        let user_2 = UserData::new();
+
+        assert_ne!(user_1.id, user_2.id);
+    }
+}
--- tests/cheese_addition_flow.rs
+++ tests/cheese_addition_flow.rs
@@ -1,6 +1,9 @@
 use std::error::Error;
 
-use cheese_wizard::{rate_cheese, CheeseRating, CheeseRatingRequest, CheeseRegistry, UserData};
+use cheese_wizard::{
+    rate_cheese, CheeseRating, CheeseRatingRequest, CheeseRegistry, RegistryCheeseRating,
+    UserCheeseRating, UserData,
+};
 
 #[test]
 fn adding_cheese_to_a_user() -> Result<(), Box<dyn Error>> {
@@ -11,10 +14,7 @@ fn adding_cheese_to_a_user() -> Result<(), Box<dyn Error>> {
     }
         "#;
 
-    let mut user = Userdata {
-        name: "Foo",
-        ..Default::default()
-    };
+    let mut user = UserData::new();
     let mut cheese_registry = CheeseRegistry::new();
 
     let cheese_rating_request: CheeseRatingRequest = serde_json::from_str(request)?;
@@ -24,6 +24,11 @@ fn adding_cheese_to_a_user() -> Result<(), Box<dyn Error>> {
     assert!(user
         .cheese_ratings
         .into_iter()
-        .all(|x| x == CheeseRating::new("FooCheeseId", 5)));
+        .all(|x| x == UserCheeseRating("FooCheeseId".into(), CheeseRating::new(5))));
+
+    assert!(cheese_registry.into_iter().all(|cheese| cheese
+        .into_iter()
+        .all(|RegistryCheeseRating(user_id, rating)| user_id == user.id
+            && rating == CheeseRating::new(5))));
     todo!("Need to finish designing cheese adding interface.");
 }
