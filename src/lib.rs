528a446ea5f377c2e712113b076b4d5c4a9a9d10
Lachlan Wilger <lachlan@wilgerfamily.com>
2023-03-15 09:40:37 -0700

    Add the ability to add ratings to UserData.

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,12 @@ pub struct UserData {
     pub id: Uuid,
     name: String,
     age: u8,
-    pub cheese_ratings: Vec<UserCheeseRating>,
+    pub cheese_ratings: UserCheeseRatingMap,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum UserDataError {
+    DuplicateCheeseName,
 }
 
 impl UserData {
@@ -21,7 +26,16 @@ impl UserData {
             id: Uuid::new_v4(),
             name: "".to_owned(),
             age: 0,
-            cheese_ratings: Vec::new(),
+            cheese_ratings: UserCheeseRatingMap::default(),
+        }
+    }
+
+    fn insert_rating(&mut self, user_rating: UserCheeseRating) -> Result<(), UserDataError> {
+        if self.cheese_ratings.0.contains_key(&user_rating.0) {
+            Err(UserDataError::DuplicateCheeseName)
+        } else {
+            self.cheese_ratings.insert(user_rating);
+            Ok(())
         }
     }
 
@@ -35,6 +49,35 @@ impl UserData {
 
     fn age(self, age: u8) -> Self {
         Self { age, ..self }
+    }
+}
+
+#[derive(PartialEq, Clone)]
+pub struct UserCheeseRating(pub String, pub CheeseRating);
+
+#[derive(Default, PartialEq, Debug, Clone)]
+pub struct UserCheeseRatingMap(HashMap<String, CheeseRating>);
+
+impl UserCheeseRatingMap {
+    fn insert(&mut self, UserCheeseRating(user_id, rating): UserCheeseRating) {
+        self.0.insert(user_id, rating);
+    }
+
+    fn get(&self, name: String) -> CheeseRating {
+        self.0[&name]
+    }
+}
+impl IntoIterator for UserCheeseRatingMap {
+    type Item = UserCheeseRating;
+    type IntoIter = std::iter::Map<
+        std::collections::hash_map::IntoIter<String, CheeseRating>,
+        fn((String, CheeseRating)) -> Self::Item,
+    >;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0
+            .into_iter()
+            .map(|(cheese_name, cheese_rating)| UserCheeseRating(cheese_name, cheese_rating))
     }
 }
 
@@ -90,9 +133,6 @@ impl CheeseRating {
         }
     }
 }
-
-#[derive(PartialEq)]
-pub struct UserCheeseRating(pub String, pub CheeseRating);
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct RegistryCheeseRating(pub Uuid, pub CheeseRating);
@@ -255,6 +295,43 @@ mod tests {
         assert_eq!(
             Err(CheeseRegistryError::DuplicateCheeseName),
             registry.insert(cheese_1)
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn inserting_a_rating_into_user_data_adds_a_rating() {
+        let mut user = UserData::new();
+        let cheese_name = "Chedder".to_owned();
+        let rating = UserCheeseRating(cheese_name.clone(), CheeseRating::new(5).unwrap());
+
+        user.insert_rating(rating);
+
+        assert_eq!(
+            user.cheese_ratings.get(cheese_name),
+            CheeseRating::new(5).unwrap()
+        );
+    }
+
+    #[test]
+    fn cheese_names_are_unique_across_user_data_ratings() -> Result<(), UserDataError> {
+        let cheese_1_rating = UserCheeseRating("Foo".to_owned(), CheeseRating::new(5).unwrap());
+        let cheese_2_rating = UserCheeseRating("Bar".to_owned(), CheeseRating::new(5).unwrap());
+
+        let mut user = UserData::new();
+
+        user.insert_rating(cheese_1_rating.clone())?;
+        user.insert_rating(cheese_2_rating.clone())?;
+
+        // The registry accepts and holds working inputs.
+        let registry_vec: Vec<UserCheeseRating> = user.cheese_ratings.clone().into_iter().collect();
+        assert!(registry_vec.contains(&cheese_1_rating));
+        assert!(registry_vec.contains(&cheese_2_rating));
+
+        assert_eq!(
+            Err(UserDataError::DuplicateCheeseName),
+            user.insert_rating(cheese_1_rating)
         );
 
         Ok(())
