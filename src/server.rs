a1c66e9b19d310b14be581592a805ad51b965cf2
Lachlan Wilger <lachlan@wilgerfamily.com>
2023-03-23 12:02:05 -0700

    /api returns an empty body with code 200

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,26 @@
-use axum::Router;
+use axum::{routing::get, Router};
 
 pub fn app() -> Router {
-    Router::new()
+    Router::new().route("/api", get(|| async { "" }))
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use tower::ServiceExt;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn api_returns_ok() {
+        let response = app()
+            .oneshot(Request::builder().uri("/api").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
 }
